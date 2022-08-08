3f8e60132324727c1723f9386710bfa5de824f0e
karaca.melihcan <karaca.melihcan@std.izu.edu.tr>
2022-08-08 22:24:06 +0300

    Starbuck landing page added

---
+++ Projects/Starbucks/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".menu-btn");
+const nav = document.getElementById("menu");
+function navToggle() {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
+}
+
+btn.addEventListener("click", navToggle);
